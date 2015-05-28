730933d2ba6c6541e05d8a6dd230a30e854bbcac
vanhungpc <vanhungpc@gmail.com>
2015-05-28 09:23:22 +0700

    Revert "Search_For_Car1"
    This reverts commit 768a67378dfc0cb6730e999fa0af370545bfd3b5.

--- src/com/dvhung/fragment/SpecsFragment.java
+++
@@ -1,7 +0,0 @@
-package com.dvhung.fragment;
-
-import android.support.v4.app.Fragment;
-
-public class SpecsFragment extends Fragment{
-
-}
--- src/com/example/search_for_car/NavigationActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.search_for_car;
-
-public class NavigationActivity {
-
-}
